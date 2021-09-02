ce21b9b4afda244aaf79c35adab8262bf0ab4a43
Avat <avatolzhobaeva@gmail.com>
2021-09-02 19:53:39 +0600

    cars added

---
+++ cars/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Car(models.Model):
+    
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cars')
+    worker = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
+                               related_name='handled_cars')
+    mark = models.ForeignKey('directory.Mark', on_delete=models.PROTECT, related_name='cars')
+    year_issue = models.IntegerField()
+    legal_number = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.legal_number
+
+
+class CarDocument(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='documents')
+    file = models.FileField()
+
+# user.cars.all()
+# blahblah = User.objects.get(user_type='specialist')
+# Car.objects.all()
+# blahblah.cars.all()
+# blahblah.handled_cars.all()
