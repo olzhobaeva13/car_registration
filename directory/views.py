c748e58b0c5ffeb043274b84f074391e9ad90f50
Avat <avatolzhobaeva@gmail.com>
2021-09-04 15:21:25 +0600

    added ModelViewSet for Mark and Brand

--- directory/views.py
+++ directory/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from directory.serializers import MarkSerializer, BrandSerializer
+from directory.models import Mark, Brand
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class MarkViewSet(ModelViewSet):
+    queryset = Mark.objects.all()
+    serializer_class = MarkSerializer
+    permission_classes = [AllowAny, ]
+
+class BrandViewSet(ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+    permission_classes = [AllowAny, ]
